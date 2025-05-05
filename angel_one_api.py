584a91aee3aa7f6e6effb12281bb04c803ff51ee
roundishmouse <amitvikram1292@gmail.com>
2025-05-05 23:40:15 +0530

    Update angel_one_api.py

--- angel_one_api.py
+++ angel_one_api.py
@@ -1,6 +1,6 @@
 import os
 import pyotp
-from smartapi.smartConnect import SmartConnect
+from Smartapi.smartConnect import SmartConnect
 
 # Read environment variables
 API_KEY = os.environ.get("SMARTAPI_API_KEY")
