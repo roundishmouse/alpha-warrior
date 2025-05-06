422e79e36be7115586aafa5c043b4969ba9a41d4
roundishmouse <amitvikram1292@gmail.com>
2025-05-07 01:52:40 +0530

    Update main.py

--- alpha_alert_bot/main.py
+++ alpha_alert_bot/main.py
@@ -7,6 +7,16 @@ app = Flask(__name__)
 def home():
     return "Alpha Warrior Bot is Live!"
 
+@app.route("/callback")
+def callback():
+    return "✅ Callback received from SmartAPI."
+
+@app.route("/my-ip")
+def get_ip():
+    import requests
+    return requests.get("https://api64.ipify.org").text
+
 if __name__ == "__main__":
+    print("⏳ Sending login request...")
     start_websocket()
     app.run(host="0.0.0.0", port=81)
