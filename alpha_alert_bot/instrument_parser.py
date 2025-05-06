0bbce1a4de7f2c5d1d2ef8a88a87e927ec7f8a69
roundishmouse <amitvikram1292@gmail.com>
2025-05-06 21:51:19 +0530

    Update instrument_parser.py

--- alpha_alert_bot/instrument_parser.py
+++ alpha_alert_bot/instrument_parser.py
@@ -2,13 +2,13 @@ import pandas as pd
 import requests
 from io import StringIO
 
-def get_nse_tokens():
-    url = "https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.csv"
+def get_token_for_symbols(symbols, exchange="NSE"):
+    url = "https://margincalc.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.csv"
     response = requests.get(url)
     if response.status_code != 200:
         raise Exception("Failed to fetch instrument master")
 
     data = pd.read_csv(StringIO(response.text))
-    data = data[data['exchange'] == 'NSE']
-    tokens = data['token'].astype(str).unique().tolist()
-    return tokens
+    data = data[(data["symbol"].isin(symbols)) & (data["exch_seg"] == exchange)]
+    return data["token"].astype(str).unique().tolist()
+
