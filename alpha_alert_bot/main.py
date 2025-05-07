fb287dea0f83cf64732fd50fd7050114e52765ab
roundishmouse <amitvikram1292@gmail.com>
2025-05-07 19:27:59 +0530

    Update main.py

--- alpha_alert_bot/main.py
+++ alpha_alert_bot/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import Flask
 from angel_one_api import start_websocket
 
@@ -19,7 +20,13 @@ def get_ip():
     import requests
     return requests.get("https://api64.ipify.org").text
 
+# New route for cron job to trigger alerts
+@app.route("/trigger-alert")
+def trigger_alert():
+    start_websocket()
+    return "Quant bot triggered by cron"
+
 if __name__ == "__main__":
-    print("⏳ Sending login request...")
+    print("✓ Sending login request...")
     start_websocket()
     app.run(host="0.0.0.0", port=81)
