3d1a84fdc65ef682214f398c2467aaad0bb631ec
roundishmouse <amitvikram1292@gmail.com>
2025-05-14 21:33:14 +0530

    Update angel_one_api.py

--- alpha_alert_bot/angel_one_api.py
+++ alpha_alert_bot/angel_one_api.py
@@ -1,7 +1,7 @@
 import os
 import requests
 import pyotp
-from smartapi.smartConnect import SmartConnect
+from smartapi_python.smartConnect import SmartConnect
 from datetime import datetime
 import json
 
