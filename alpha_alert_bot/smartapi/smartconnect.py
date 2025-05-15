729965f77987db9cbd0898ad6a3730d7efbde904
roundishmouse <amitvikram1292@gmail.com>
2025-05-15 21:43:43 +0530

    Update smartconnect.py

--- alpha_alert_bot/smartapi/smartconnect.py
+++ alpha_alert_bot/smartapi/smartconnect.py
@@ -16,7 +16,9 @@ class SmartConnect:
         self.client_code = client_code
         now = datetime.datetime.utcnow()
         totp = pyotp.TOTP(totp_secret)
-        generated_totp = totp.at(now)
+        timestamp = int(datetime.datetime.utcnow().timestamp())
+        generated_totp = totp.at(timestamp)
+
 
         print("System Time (UTC):", now)
         print("TOTP being used by bot:", generated_totp)
