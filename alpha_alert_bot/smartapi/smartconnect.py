4ff439929657f5a51bd4f7968835df7e55b56a5e
roundishmouse <amitvikram1292@gmail.com>
2025-05-15 09:30:59 +0530

    Update smartconnect.py

--- alpha_alert_bot/smartapi/smartconnect.py
+++ alpha_alert_bot/smartapi/smartconnect.py
@@ -1,4 +1,6 @@
 import pyotp
+import time
+import requests
 
 class SmartConnect:
     def __init__(self, api_key):
@@ -6,18 +8,43 @@ class SmartConnect:
         self.jwt_token = None
         self.feed_token = None
         self.refresh_token = None
+        self.client_code = None
+        self.session_expiry = None
 
     def generateSession(self, client_code, pin, totp_secret):
+        self.client_code = client_code
         totp = pyotp.TOTP(totp_secret).now()
-        
-        print(f"[DUMMY LOGIN] Logging in with: API_KEY={self.api_key}, CLIENT_CODE={client_code}, PIN={pin}, TOTP={totp}")
-        
-        self.jwt_token = "mocked_jwt_token"
-        self.feed_token = "mocked_feed"
-        self.refresh_token = "mocked_refresh"
-        
-        return {
-            "data": {
-                "jwtToken": self.jwt_token
-            }
+
+        payload = {
+            "clientcode": client_code,
+            "password": pin,
+            "totp": totp
         }
+
+        headers = {
+            "Content-Type": "application/json",
+            "X-Api-Key": self.api_key
+        }
+
+        url = "https://apiconnect.angelbroking.com/rest/auth/angelbroking/user/v1/loginByPassword"
+
+        response = requests.post(url, json=payload, headers=headers)
+        data = response.json()
+
+        if response.status_code == 200 and data.get("status"):
+            self.jwt_token = data["data"]["jwtToken"]
+            self.feed_token = data["data"]["feedToken"]
+            self.refresh_token = data["data"]["refreshToken"]
+            self.session_expiry = time.time() + 7200
+            return data["data"]
+        else:
+            raise Exception(f"Login failed: {data.get('message', 'Unknown error')}")
+
+    def getJwtToken(self):
+        return self.jwt_token
+
+    def getFeedToken(self):
+        return self.feed_token
+
+    def getClientCode(self):
+        return self.client_code
