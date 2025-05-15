ac85f536d0e0569d9851106cc782df76f5ab8cfe
roundishmouse <amitvikram1292@gmail.com>
2025-05-15 20:55:19 +0530

    Update smartconnect.py

--- alpha_alert_bot/smartapi/smartconnect.py
+++ alpha_alert_bot/smartapi/smartconnect.py
@@ -29,7 +29,7 @@ class SmartConnect:
         payload = {
             "clientcode": client_code,
             "password": pin,
-            "totp": totp
+            "totp": totp.now()
         }
 
         headers = {
