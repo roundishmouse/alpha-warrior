4b7b9064e899f26bedae2b31a3f9480e65f7ef10
roundishmouse <amitvikram1292@gmail.com>
2025-05-15 23:21:43 +0530

    Update smartconnect.py

--- alpha_alert_bot/smartapi/smartconnect.py
+++ alpha_alert_bot/smartapi/smartconnect.py
@@ -2,7 +2,7 @@ import pyotp
 import time
 import requests
 import datetime
-import calendar
+from ntplib import NTPClient  # New import for accurate time
 
 class SmartConnect:
     def __init__(self, api_key):
@@ -13,18 +13,25 @@ class SmartConnect:
         self.client_code = None
         self.session_expiry = None
 
+    def get_ntp_utc(self):
+        try:
+            client = NTPClient()
+            response = client.request('pool.ntp.org')
+            return datetime.datetime.utcfromtimestamp(response.tx_time)
+        except Exception as e:
+            print("NTP sync failed. Falling back to system UTC.", e)
+            return datetime.datetime.utcnow()
+
     def generateSession(self, client_code, pin, totp_secret):
         self.client_code = client_code
 
-        # Get exact UTC time and convert to timestamp
-        now = datetime.datetime.utcnow()
-        timestamp = calendar.timegm(now.timetuple())
+        # Sync UTC time using NTP
+        now = self.get_ntp_utc()
+        print("NTP Time (UTC):", now)
 
-        print("System Time (UTC):", now)
-
-        # Generate TOTP using the injected timestamp
+        # Generate TOTP using synced UTC time
         totp = pyotp.TOTP(totp_secret)
-        generated_totp = totp.at(timestamp)
+        generated_totp = totp.at(int(now.timestamp()))
         print("TOTP being used by bot:", generated_totp)
 
         payload = {
@@ -39,6 +46,7 @@ class SmartConnect:
         }
 
         url = "https://apiconnect.angelbroking.com/rest/auth/angelbroking/user/v1/loginByPassword"
+
         response = requests.post(url, json=payload, headers=headers)
         data = response.json()
         print(">>> RESPONSE:", response.status_code, data)
@@ -47,5 +55,6 @@ class SmartConnect:
             self.jwt_token = data["data"]["jwtToken"]
             self.feed_token = data["data"]["feedToken"]
             self.refresh_token = data["data"]["refreshToken"]
+            self.session_expiry = data["data"]["expiry"]
         else:
-            print("Login failed.")
+            print("Login failed. JWT:", self.jwt_token)
