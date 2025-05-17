793f32579cc99f8a19cfe6a0f1c0d0d067a70466
roundishmouse <amitvikram1292@gmail.com>
2025-05-17 07:57:40 +0530

    Update main.py

--- alpha_alert_bot/main.py
+++ alpha_alert_bot/main.py
@@ -17,7 +17,7 @@ obj = SmartConnect(api_key)
 
 # Perform login with positional arguments
 try:
-    data = obj.generateSession(clientCode=client_code, password=password, totp_secret=totp_secret)
+    data = obj.generateSession(client_code, password, totp)
     print("Login successful!")
     print("JWT Token:", data['data']['jwtToken'])
 except Exception as e:
