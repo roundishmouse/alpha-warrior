285e3ca47f8708e37bb0b977e67beea67cd77bb8
roundishmouse <amitvikram1292@gmail.com>
2025-05-17 23:25:21 +0530

    Update main.py

--- alpha_alert_bot/main.py
+++ alpha_alert_bot/main.py
@@ -2,7 +2,7 @@ from smartapi import SmartConnect
 import pyotp
 import os
 
-# Environment variables
+# Environment variables (set on Render)
 api_key = os.getenv("SMARTAPI_API_KEY")
 client_code = os.getenv("SMARTAPI_CLIENT_CODE")
 password = os.getenv("SMARTAPI_PASSWORD")
@@ -15,9 +15,9 @@ print("Generated TOTP:", totp)
 # Initialize SmartConnect
 obj = SmartConnect(api_key=api_key)
 
-# Login with only 3 args
+# Perform login
 try:
-    data = obj.generateSession(client_code, password, totp)  # Only 3 args
+    data = obj.generateSession(client_code, password, totp)
     print("Login successful!")
     print("JWT Token:", data['data']['jwtToken'])
 except Exception as e:
