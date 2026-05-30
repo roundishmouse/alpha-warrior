788a5540516850763e754ed0983287d40219f75f
roundishmouse <amitvikram1292@gmail.com>
2026-05-30 20:57:16 +0530

    Update main.py
    UPDATED IP

--- alpha_alert_bot/main.py
+++ alpha_alert_bot/main.py
@@ -390,4 +390,4 @@ def run_bot():
 # ============================================================
 if __name__ == "__main__":
     threading.Thread(target=run_bot).start()
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="127.0.0.1", port=10000)
